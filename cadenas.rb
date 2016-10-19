44ada8a3e45e2ef987c19d83af75ea4c837423a4
Alejandro Miranda Genaro <alexmiragen@gmail.com>
2016-10-19 17:45:52 -0500

    programas de prueba

---
+++ cadenas.rb
@@ -0,0 +1,17 @@
+cadena = "nombre"
+cadena1 = "Taco"
+puts cadena[1]
+puts cadena.capitalize
+cadena1[0]="P"
+puts cadena1
+puts 81.chr
+puts "alejandro".count "o"
+puts "vacio".empty?
+
+puts "tuyo".sub('t','s')
+puts "hello".gsub('l','!')
+puts "Bienvenido".include? "si"
+puts "world".index ('d')
+puts "Alejandro".reverse
+puts "Bienvenidos al Curso de Programación".split(//)
+puts "    Bienvenidos al Curso de Programación    ".strip
---
+++ conkt.rb
@@ -0,0 +1,13 @@
+def sum_words (text1, text2)
+	puts "Tus cadenas concatenadas son: #{text1} + #{text2}"
+end
+
+
+puts "Introduce una primer cadena"
+cad1 = gets.chomp
+puts "Introduce una segunda cadena"
+cad2 = gets.chomp
+
+sum_words(cad1, cad2)
+
+
