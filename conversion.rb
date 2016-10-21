1e8cf9575b588439453a8ad51161d0ae7711e6fd
Alejandro Miranda Genaro <alexmiragen@gmail.com>
2016-10-20 20:05:39 -0500

    Ejercicio resuelto x.to_f

---
+++ conversion.rb
@@ -0,0 +1,12 @@
+num1 = 7
+num2 = 30
+sum = num1 + num2
+puts sum
+difference = num2 - num1
+puts difference
+product = num1 * num2
+puts product
+quotient = num2.to_f / num1.to_f
+puts quotient
+modulus = num2.to_f % num1.to_f
+puts modulus
